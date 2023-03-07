6cedca93dc0dfaead3880312faa738bf42180066
Raddy <rx3mer@live.co.uk>
2023-03-07 17:06:12 +0000

    Mongoose 7.0.0 Update
    Fixed error on Mongoose 7.0.0 MongooseError: Query.prototype.exec() no longer accepts a callback

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -16,7 +16,8 @@ exports.dashboard = async (req, res) => {
   };
 
   try {
-    Note.aggregate([
+    // Mongoose "^7.0.0 Update
+    const notes = await Note.aggregate([
       { $sort: { updatedAt: -1 } },
       { $match: { user: mongoose.Types.ObjectId(req.user.id) } },
       {
@@ -24,23 +25,50 @@ exports.dashboard = async (req, res) => {
           title: { $substr: ["$title", 0, 30] },
           body: { $substr: ["$body", 0, 100] },
         },
-      },
-    ])
-      .skip(perPage * page - perPage)
-      .limit(perPage)
-      .exec(function (err, notes) {
-        Note.count().exec(function (err, count) {
-          if (err) return next(err);
-          res.render("dashboard/index", {
-            userName: req.user.firstName,
-            locals,
-            notes,
-            layout: "../views/layouts/dashboard",
-            current: page,
-            pages: Math.ceil(count / perPage),
-          });
-        });
-      });
+      }
+      ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec(); 
+
+    const count = await Note.count();
+
+    res.render('dashboard/index', {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage)
+    });
+ 
+    // Original Code
+    // Note.aggregate([
+    //   { $sort: { updatedAt: -1 } },
+    //   { $match: { user: mongoose.Types.ObjectId(req.user.id) } },
+    //   {
+    //     $project: {
+    //       title: { $substr: ["$title", 0, 30] },
+    //       body: { $substr: ["$body", 0, 100] },
+    //     },
+    //   },
+    // ])
+    //   .skip(perPage * page - perPage)
+    //   .limit(perPage)
+    //   .exec(function (err, notes) {
+    //     Note.count().exec(function (err, count) {
+    //       if (err) return next(err);
+    //       res.render("dashboard/index", {
+    //         userName: req.user.firstName,
+    //         locals,
+    //         notes,
+    //         layout: "../views/layouts/dashboard",
+    //         current: page,
+    //         pages: Math.ceil(count / perPage),
+    //       });
+    //     });
+    //   });
+
   } catch (error) {
     console.log(error);
   }
