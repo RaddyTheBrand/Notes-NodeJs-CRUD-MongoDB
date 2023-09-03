10cb88449e1a3c637719487c49b4a1b1464c5d78
Raddy <rx3mer@live.co.uk>
2023-09-03 20:20:53 +0100

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -74,10 +74,21 @@ passport.serializeUser(function (user, done) {
 });
 
 // Retrieve user data from session.
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
+// Old
+// passport.deserializeUser(function (id, done) {
+//   User.findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
+
+// New
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 
